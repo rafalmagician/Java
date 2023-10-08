138095d27ff8107a26c57e10e763c76738851928
Rafal Walasek <rafal_walasek@interia.pl>
2023-10-08 21:18:40 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        WyszukiwanieLiniowe wyszukiwanieLiniowe = new WyszukiwanieLiniowe();
+        wyszukiwanieLiniowe.wyswietlenie();
+
+        
+
+    }
+
+}
