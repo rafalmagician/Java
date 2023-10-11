2959d5ffef65639fb84b3707c0cd55e7b226b6b0
Rafal Walasek <rafal_walasek@interia.pl>
2023-10-11 21:41:42 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -6,33 +6,18 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        WyszukiwanieLiniowe wyszukiwanieLiniowe = new WyszukiwanieLiniowe();
-        ZliczanieWgKryterium zliczanieWgKryterium = new ZliczanieWgKryterium();
-        WyszukiwanieMaxMin wyszukiwanieMaxMin = new WyszukiwanieMaxMin();
-        JednoczesneWyszukiwanieMaxMin jednoczesneWyszukiwanieMaxMin = new JednoczesneWyszukiwanieMaxMin();
-        SortowaniePrzezWybor sortowaniePrzezWybor = new SortowaniePrzezWybor();
-        NajczestszaWartoscWZbiorze najczestszaWartoscWZbiorze = new NajczestszaWartoscWZbiorze();
+        TabliceWektory tabliceWektory = new TabliceWektory();
 
         boolean choise = false;
         while (!choise) {
 
-            System.out.println("Wybierz opcję z menu: ");
-            System.out.println("0 -> Wyjście");
-            System.out.println("1 -> Wyszukiwanie liniowe");
-            System.out.println("2 -> Zliczanie wg kryterium");
-            System.out.println("3 -> Wyszukiwanie max i min");
-            System.out.println("4 -> Jednoczesne wyszukiwanie max i min");
-            System.out.println("5 -> Sortowanie przez wybór");
-            System.out.println("6 -> Najczęstsza wartość w zbiorze");
-            System.out.print("-> ");
+            System.out.println("=====  MENU  =====");
+            System.out.println("0 -> WYJŚCIE");
+            System.out.println("1 -> PRZEDZIAŁY LICZBOWE I LICZBY");
+            System.out.println("2 -> TABLICE - WEKTORY");
             switch (scanner.nextInt()) {
                 case 0 -> choise = true;
-                case 1 -> wyszukiwanieLiniowe.wyswietlenie();
-                case 2 -> zliczanieWgKryterium.wyswietlanie();
-                case 3 -> wyszukiwanieMaxMin.wypisanie();
-                case 4 -> jednoczesneWyszukiwanieMaxMin.wypisanie();
-                case 5 -> sortowaniePrzezWybor.wyswietlenieDanych();
-                case 6 -> najczestszaWartoscWZbiorze.wypisz();
+                case 2 -> tabliceWektory.display();
             }
 
         }
