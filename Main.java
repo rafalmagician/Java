1db73eb1c823407163e958ff8a4c9703c122acac
Rafal Walasek <rafal_walasek@interia.pl>
2023-10-14 22:23:37 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,12 +1,14 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         Scanner scanner = new Scanner(System.in);
 
         TabliceWektory tabliceWektory = new TabliceWektory();
+        Pliki pliki = new Pliki();
 
         boolean choise = false;
         while (!choise) {
@@ -15,9 +17,12 @@ public class Main {
             System.out.println("0 -> WYJŚCIE");
             System.out.println("1 -> PRZEDZIAŁY LICZBOWE I LICZBY");
             System.out.println("2 -> TABLICE - WEKTORY");
+            System.out.println("3 -> PLIKI");
+            System.out.print("-> ");
             switch (scanner.nextInt()) {
                 case 0 -> choise = true;
                 case 2 -> tabliceWektory.display();
+                case 3 -> pliki.display();
             }
 
         }
---
+++ Pliki.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Pliki {
+    private File file;
+    private Scanner scanner = new Scanner(System.in);
+
+    public void display() throws IOException {
+        System.out.print("Podaj nazwę pliku i rozszerzenie 'np. plik.txt': ");
+        file = new File(fileName());
+        System.out.println("Czy plik istnieje: " + isFileExists());
+        createFile();
+    }
+
+    private boolean isFileExists() {
+        return file.exists();
+    }
+
+    private String fileName() {
+        return scanner.nextLine();
+    }
+    private void createFile() throws IOException {
+        if (!isFileExists()) {
+            boolean success = file.createNewFile();
+            System.out.println("Plik został utworzony: " + success);
+        } else {
+            System.out.println("Plik już istnieje");
+        }
+        System.out.println();
+    }
+}
