9db6df65dc4abd71eb11bf7216eae4b1e10c5127
Rafal Walasek <rafal_walasek@interia.pl>
2023-10-15 20:59:10 +0200

    Add files via upload

--- Pliki.java
+++ Pliki.java
@@ -1,17 +1,35 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Pliki {
     private File file;
     private Scanner scanner = new Scanner(System.in);
+    private String wybor = "t";
 
     public void display() throws IOException {
-        System.out.print("Podaj nazwę pliku i rozszerzenie 'np. plik.txt': ");
-        file = new File(fileName());
-        System.out.println("Czy plik istnieje: " + isFileExists());
-        createFile();
-        deleteFile();
+        System.out.print("Czy chcesz utworzyć plik 't/n': ");
+        if (wybor.equals(scanner.nextLine())) {
+            System.out.print("Podaj nazwę pliku i rozszerzenie 'np. plik.txt': ");
+            file = new File(fileName());
+            createFile();
+        } else {
+            System.out.println("Usunięcie pliku: ");
+            deleteFile();
+            System.out.println("Odczyt z pliku: ");
+            odczytPliku();
+        }
+        System.out.println();
+    }
+
+    private void createFile() throws IOException {
+        if (!isFileExists()) {
+            boolean success = file.createNewFile();
+            System.out.println(success + " Plik został utworzony pomyślnie");
+        } else {
+            System.out.println("Plik nie został utworzony. Plik istnieje");
+        }
     }
 
     private boolean isFileExists() {
@@ -21,22 +39,26 @@ public class Pliki {
     private String fileName() {
         return scanner.nextLine();
     }
-    private void createFile() throws IOException {
-        if (!isFileExists()) {
-            boolean success = file.createNewFile();
-            System.out.println("Plik został utworzony: " + success);
-        } else {
-            System.out.println("Plik już istnieje");
-        }
-        System.out.println();
-    }
 
     private void deleteFile() {
         System.out.print("Czy chcesz usunąć plik 't/n': ");
-        if ("T".equals(scanner.nextLine())) {
+        if ("t".equals(scanner.nextLine())) {
+            System.out.print("Podaj nazwę pliku do usunięcia: ");
+            file = new File(fileName());
             System.out.println("Plik został usunięty " + file.delete());
         } else {
             System.out.println("Plik NIE został usunięty");
         }
+    }
+
+    private void zapisDoPliku() {
+
+    }
+
+    private void odczytPliku() throws FileNotFoundException {
+        System.out.print("Podaj nazwę pliku do odczytania: ");
+        file = new File(fileName());
+        Scanner odczyt = new Scanner(file);
+        System.out.println(odczyt.nextLine());
     }
 }
