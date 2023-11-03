0f4badb027fb301067d066719bbc15aa204500e9
Rafal Walasek <rafal_walasek@interia.pl>
2023-11-03 20:37:58 +0100

    Add files via upload

--- SPOJ/DwieCyfrySilni.java
+++ SPOJ/DwieCyfrySilni.java
@@ -3,26 +3,50 @@ package SPOJ;
 import java.util.Scanner;
 
 public class DwieCyfrySilni {
-    private int n;
     private Scanner scanner = new Scanner(System.in);
 
     public void silnia() {
-        System.out.print("Podaj liczbe calkowita: ");
-        n = scanner.nextInt();
+        int liczba = scanner.nextInt();
 
-        int wynik = 1;
-        for (int i = 1; i <= n; i++) {
-            wynik *= i;
+        if (liczba > 9) {
+            System.out.println("0 0");
+        }
+        if (liczba <= 9){
+            int wynik = 1;
+            for (int i = 1; i <= liczba; i++) {
+                wynik *= i;
+            }
+
+            int jednosci = wynik % 10;
+            int dziesiatki = (wynik / 10) % 10;
+            System.out.println(dziesiatki + " " + jednosci);
         }
 
-        System.out.println(n + "! = " + wynik);
-    }
+        //System.out.println(wynik);
 
-    public static long silnia(long liczba) {
-        if (liczba == 0) {
-            return 1;
-        } else {
-            return liczba * silnia(liczba - 1);
+        //w javie nie ma typu ktory obsluzy wieksze liczby niz: long wiec trzeba skozystac z klasy: BigInteger!
+        //kod dziala ale jest bledna odpowiedz...
+        //...sprobuje z klasa BigInteger...
+
+        //int liczba = scanner.nextInt()
+        //BigInteger silnia = ;
+
+        /*Scanner scanner = new Scanner(System.in);
+        int liczba = scanner.nextInt();
+        BigInteger mod = new BigInteger("10");
+
+        if (liczba > 9) {
+            System.out.println("0 0");
         }
+        if (liczba <= 9){
+            BigInteger wynik = new BigInteger("1");
+            for (int i = 1; i <= liczba; i++) {
+                wynik = wynik.multiply(BigInteger.valueOf(i));
+            }
+
+            BigInteger jednosci = wynik.remainder(mod);
+            BigInteger dziesiatki = (wynik.divide(mod)).remainder(mod);
+            System.out.println(dziesiatki + " " + jednosci);
+        }*/
     }
 }
