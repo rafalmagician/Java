ee2a7027a56051da6487501b7cf3e0db7c451f25
Rafal Walasek <rafal_walasek@interia.pl>
2023-11-03 08:03:09 +0100

    Add files via upload

--- ligaPilkaNozna/League_1.java
+++ ligaPilkaNozna/League_1.java
@@ -10,18 +10,19 @@ import java.util.List;
 import java.util.Scanner;
 
 public class League_1 {
-    private final Team kamien = new Team("Kamien", 0, 0, 0, 15);
-    private final Team elektro = new Team("Elektro", 0, 0, 0, 7);
-    private final Team roslina = new Team("Roslina", 0, 0, 0, 3);
-    private final Team walka = new Team("Walka", 0, 0, 0, 10);
-    private final Team ziemia = new Team("Ziemia", 0, 0, 0, 12);
-    private final Team ogien = new Team("Ogien", 0, 0, 0, 5);
-    private final Team spinery = new Team("Spinery", 0, 0, 0, 13);
-    private final Team karty = new Team("Karty", 0, 0, 0, 17);
+    private final Team kamien = new Team("Kamien", 0, 0, 0, 0);
+    private final Team elektro = new Team("Elektro", 0, 0, 0, 0);
+    private final Team roslina = new Team("Roslina", 0, 0, 0, 0);
+    private final Team walka = new Team("Walka", 0, 0, 0, 0);
+    private final Team ziemia = new Team("Ziemia", 0, 0, 0, 0);
+    private final Team ogien = new Team("Ogien", 0, 0, 0, 0);
+    private final Team spinery = new Team("Spinery", 0, 0, 0, 0);
+    private final Team karty = new Team("Karty", 0, 0, 0, 0);
 
     private final List<Team> teamsOne = new ArrayList<>();
     private File file;
     private PrintWriter printWriter;
+    private Scanner scaner = new Scanner(System.in);
 
     public void addTeamsOneLeague() {
         teamsOne.add(kamien);
@@ -32,6 +33,38 @@ public class League_1 {
         teamsOne.add(ogien);
         teamsOne.add(spinery);
         teamsOne.add(karty);
+    }
+
+    public void mecz() {
+        System.out.println(kamien.getTeamName() + " - " + elektro.getTeamName());
+        System.out.println("Wynik");
+
+        kamien.setGame(1);
+        elektro.setGame(1);
+
+        System.out.print("Gole Kamien: ");
+        int k = Integer.valueOf(scaner.nextLine());
+        kamien.setGainGoal(k);
+        elektro.setLoseGoal(k);
+        System.out.print("Gole Elektro: ");
+        int e = Integer.valueOf(scaner.nextLine());
+        elektro.setGainGoal(e);
+        kamien.setLoseGoal(e);
+
+        if (k > e) {
+            kamien.setPkt(3);
+            elektro.setPkt(0);
+        } else if (k < e) {
+            kamien.setPkt(0);
+            elektro.setPkt(3);
+        } else {
+            kamien.setPkt(1);
+            elektro.setPkt(1);
+        }
+
+        System.out.println(kamien.getTeamName() + " " + kamien.getGainGoal() + ":" + elektro.getGainGoal() + " " + elektro.getTeamName());
+
+        System.out.println();
     }
 
     public void tableInformationLeagueOne() {
