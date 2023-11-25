b5d3a0a0920c7140747172376beb15a07a99f161
Rafal Walasek <rafal_walasek@interia.pl>
2023-11-25 11:54:46 +0100

    Add files via upload

--- GUI/Main.java
+++ GUI/Main.java
@@ -1,10 +1,14 @@
 package GUI;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
-
+        Okienko okienko = new Okienko();
+        okienko.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        okienko.setVisible(true);
 
     }
 
---
+++ GUI/Okienko.java
@@ -0,0 +1,12 @@
+package GUI;
+
+import javax.swing.*;
+
+public class Okienko extends JFrame {
+
+    public Okienko() {
+        setSize(700, 600);
+        setTitle("Moje pierwsze okienko");
+    }
+
+}
