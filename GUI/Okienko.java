ee9c0ccfc7c15513a3cda3598115bf0d13e1fe32
Rafal Walasek <rafal_walasek@interia.pl>
2023-11-25 21:22:12 +0100

    Add files via upload

--- GUI/Okienko.java
+++ GUI/Okienko.java
@@ -1,12 +1,42 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
 
-public class Okienko extends JFrame {
+public class Okienko extends JFrame implements ActionListener {
+
+    private JButton bPodajDate = new JButton("podaj date");
+    private JButton bWyjscie = new JButton("wyjscie");
+    private JLabel lWyswietlDate = new JLabel("Data: ");
 
     public Okienko() {
-        setSize(700, 600);
+        setSize(300, 300);
         setTitle("Moje pierwsze okienko");
+        setLayout(null);
+
+        bPodajDate.setBounds(50, 50, 100, 50);
+        add(bPodajDate);
+        bPodajDate.addActionListener(this);
+
+        bWyjscie.setBounds(150, 50, 100, 50);
+        add(bWyjscie);
+        bWyjscie.addActionListener(this);
+
+        lWyswietlDate.setBounds(50, 150, 200, 50);
+        add(lWyswietlDate);
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        Object zrodlo = e.getSource();
+        if (zrodlo.equals(bPodajDate)) {
+            lWyswietlDate.setText(new Date().toString());
+        } else if (zrodlo.equals(bWyjscie)){
+            dispose();
+        }
+
+    }
 }
