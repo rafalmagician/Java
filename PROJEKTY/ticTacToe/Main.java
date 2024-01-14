81d7ab5157de2d94d0757cc1e574f277e2313730
Rafal Walasek <rafal_walasek@interia.pl>
2024-01-14 10:50:52 +0100

    Add files via upload

--- PROJEKTY/ticTacToe/Main.java
+++ PROJEKTY/ticTacToe/Main.java
@@ -1,27 +1,41 @@
 package PROJEKTY.ticTacToe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        int[][] tab = new int[3][3];
-        int count = 0;
+        //wspolrzedne uzytkownika
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj wspolrzedne (gdzie wstawic znak): ");
+        System.out.print("Wsp 1: ");
+        int wsp1 = scanner.nextInt();
+        System.out.print("Wsp 2: ");
+        int wsp2 = scanner.nextInt();
 
-        System.out.println("   a   b   c");
-        for (int i = 0; i < tab.length; i++) {
-            System.out.print(count++ + "  ");
-            for (int j = 0; j < tab[i].length; j++) {
-                if (j == 2) {
-                    System.out.println(tab[i][j]);
-                } else {
-                    System.out.print(tab[i][j] + " | ");
-                }
-            }
-            if (i == 2) {
-                System.out.println();
-            } else {
-                System.out.println("  -----------");
-            }
-        }
+        System.out.println("Wspolrzedne: " + "wsp1 = " + wsp1 + "; wsp2 = " + wsp2);
+
+
+        //wyswietlenie tabeli
+//        int[][] tab = new int[3][3];
+//        int count = 1;
+//
+//        System.out.println("   1   2   3");
+//        for (int i = 0; i < tab.length; i++) {
+//            System.out.print(count++ + "  ");
+//            for (int j = 0; j < tab[i].length; j++) {
+//                if (j == 2) {
+//                    System.out.println(tab[i][j]);
+//                } else {
+//                    System.out.print(tab[i][j] + " | ");
+//                }
+//            }
+//            if (i == 2) {
+//                System.out.println();
+//            } else {
+//                System.out.println("  -----------");
+//            }
+//        }
 
     }
 }
